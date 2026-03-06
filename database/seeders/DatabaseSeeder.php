8106aa862537116aa27e2daaf80bd08ca700bf2e
ahan-vian <farhanvian346@gmail.com>
2026-03-06 14:18:28 +0700

    feat(authz): add roles/permissions and protect routes with spatie middleware

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,11 +5,14 @@ namespace Database\Seeders;
 use App\Models\Location;
 use App\Models\Sensor;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $this->call(RolesAndPermissionsSeeder::class);
         Location::factory()
             ->count(10)
             ->create();
@@ -17,5 +20,20 @@ class DatabaseSeeder extends Seeder
         Sensor::factory()
             ->count(50)
             ->create();
+
+        User::updateOrCreate(
+            ['email' => 'admin@test.com'],
+            ['name' => 'Admin', 'password' => Hash::make('password123')]
+        )->assignRole('admin');
+
+        User::updateOrCreate(
+            ['email' => 'tech@test.com'],
+            ['name' => 'Technician', 'password' => Hash::make('password123')]
+        )->assignRole('technician');
+
+        User::updateOrCreate(
+            ['email' => 'viewer@test.com'],
+            ['name' => 'Viewer', 'password' => Hash::make('password123')]
+        )->assignRole('viewer');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,18 @@ Route::prefix('auth')->group(function () {
 
 // protect resources
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('locations', LocationController::class);
-    Route::apiResource('sensors', SensorController::class);
+
+    // Locations
+    Route::get('/locations', [LocationController::class, 'index'])->middleware('permission:locations.read');
+    Route::get('/locations/{location}', [LocationController::class, 'show'])->middleware('permission:locations.read');
+    Route::post('/locations', [LocationController::class, 'store'])->middleware('permission:locations.manage');
+    Route::put('/locations/{location}', [LocationController::class, 'update'])->middleware('permission:locations.manage');
+    Route::delete('/locations/{location}', [LocationController::class, 'destroy'])->middleware('permission:locations.manage');
+
+    // Sensors
+    Route::get('/sensors', [SensorController::class, 'index'])->middleware('permission:sensors.read');
+    Route::get('/sensors/{sensor}', [SensorController::class, 'show'])->middleware('permission:sensors.read');
+    Route::post('/sensors', [SensorController::class, 'store'])->middleware('permission:sensors.manage');
+    Route::put('/sensors/{sensor}', [SensorController::class, 'update'])->middleware('permission:sensors.manage');
+    Route::delete('/sensors/{sensor}', [SensorController::class, 'destroy'])->middleware('permission:sensors.delete');
 });
