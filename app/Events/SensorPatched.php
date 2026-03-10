7b5ee01e23aced992a44c06907479ba8ed368d6e
ahan-vian <farhanvian346@gmail.com>
2026-03-10 11:25:40 +0700

    refactor(event): process sensor patched listener async vie queue

--- app/Events/SensorPatched.php
+++ app/Events/SensorPatched.php
@@ -2,35 +2,22 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use App\Models\Sensor;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class SensorPatched
 {
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+    use Dispatchable, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+    public Sensor $sensor;
+    public ?int $userId;
+    public array $changes;
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
+    public function __construct(Sensor $sensor, ?int $userId, array $changes)
     {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
+        $this->sensor = $sensor;
+        $this->userId = $userId;
+        $this->changes = $changes;
     }
 }
--- app/Listeners/LogSensorPatchedListener.php
+++ app/Listeners/LogSensorPatchedListener.php
@@ -4,9 +4,13 @@ namespace App\Listeners;
 
 use App\Events\SensorPatched;
 use App\Models\ActivityLog;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
 
-class LogSensorPatchedListener
+class LogSensorPatchedListener implements ShouldQueue
 {
+    use InteractsWithQueue;
+
     public function handle(SensorPatched $event): void
     {
         ActivityLog::create([
